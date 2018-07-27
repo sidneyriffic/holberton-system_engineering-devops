0aa77866a43f42fa1d9ee1e27cdb8fa7e7a02a28
Sidney Burkett <sidneyriffic@gmail.com>
2018-07-26 19:13:34 -0700

    Add 0x0D-1 and lint 0x0D-0

--- 0x0D-configuration_management/0-create_a_file.pp
+++ 0x0D-configuration_management/0-create_a_file.pp
@@ -1,6 +1,7 @@
+#Creates /tmp/holberton with the given attributes
 file { '/tmp/holberton':
-  owner => www-data,
-  group => www-data,
-  mode => "0744",
-  content => "I love Puppet"
+  owner   => www-data,
+  group   => www-data,
+  mode    => '0744',
+  content => 'I love Puppet'
 }
---
+++ 0x0D-configuration_management/1-install_a_package.pp
@@ -0,0 +1,5 @@
+#installs puppet-lint
+package { 'puppet-lint':
+  ensure   => '1.1.0',
+  provider => 'gem',
+}
